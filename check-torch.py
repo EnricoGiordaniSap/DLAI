310720e9ea0ab91be0999add34731658d88722de
enrico00.giordani <enrico00.giordani@gmail.com>
2026-02-27 15:41:29 +0100

    fundamentals

--- check-torch.py
+++ check-torch.py
@@ -1,8 +1,34 @@
 import torch
 
-print(f"Versione PyTorch: {torch.__version__}")
-print(f"GPU disponibile: {torch.cuda.is_available()}")
+scalar= torch.tensor(7)
+# print(scalar.item())
 
-if torch.cuda.is_available():
-    print(f"Nome GPU: {torch.cuda.get_device_name(0)}")
-    print(f"Versione CUDA di Torch: {torch.version.cuda}")
+vector= torch.tensor([7,7])
+# print(vector.shape)
+
+MATRIX = torch.tensor([
+    [7,8],
+    [9,10]
+])
+print(MATRIX.shape)
+
+# Tensor
+TENSOR = torch.tensor([[[1, 2, 3],
+                        [3, 6, 9],
+                        [2, 4, 5]]])
+# print(TENSOR)
+
+print(TENSOR.shape)
+
+# Create a random tensor of size (3, 4)
+random_tensor = torch.rand(size=(3, 4))
+# print(random_tensor, random_tensor.dtype)
+
+# Create a tensor of all zeros
+zeros = torch.zeros(size=(3, 4))
+# print(zeros, zeros.dtype)
+
+# Create a tensor of all ones
+ones = torch.ones(size=(3, 4))
+# print (ones, ones.dtype)
+
